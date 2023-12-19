b46d950fee7d826b172e0526e93ab88479af47b4
Olsen Kraja <krajaolsen@gmail.com>
2023-12-19 11:47:45 +0100

    Initial design commit

--- app/(site)/horses/[slug]/page.tsx
+++ app/(site)/horses/[slug]/page.tsx
@@ -13,41 +13,41 @@ export default async function HorsePage({params}: { params: { slug: string } }) 
   const horsePosts = allPosts.filter((post) => post.entry.horses.includes(slug))
   return (
     <div className="mx-auto max-w-screen-xl mt-12 mb-32 px-8">
-        <div className="prose lg:prose-xl">
-          <h1>{horse.name}</h1>
-          {horse.showcase.length > 0 && (
-            <>
-              <hr />
-              <h2>Showcase</h2>
-              <ul>
-                {horse.showcase.map((item, index) => (
-                  <li key={index}>
-                    {item.discriminant === 'link' && (
-                      <ShowcaseLink url={item.value.url} label={item.value.label} />
-                    )}
-                    {item.discriminant === 'youtubeVideoId' && (
-                      <ShowcaseYouTubeVideo videoId={item.value} />
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
-          {horsePosts.length > 0 && (
-            <>
-              <hr />
-              <h2>Posts über {horse.name}</h2>
-              <ul>
-                {horsePosts.map((post) => (
-                  <li key={post.slug}>
-                    <Link href={`/${post.slug}`}>{post.entry.title}</Link>
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
-        </div>
+      <div className="prose lg:prose-xl">
+        <h1>{horse.name}</h1>
+        {horse.showcase.length > 0 && (
+          <>
+            <hr />
+            <h2>Showcase</h2>
+            <ul>
+              {horse.showcase.map((item, index) => (
+                <li key={index}>
+                  {item.discriminant === 'link' && (
+                    <ShowcaseLink url={item.value.url} label={item.value.label} />
+                  )}
+                  {item.discriminant === 'youtubeVideoId' && (
+                    <ShowcaseYouTubeVideo videoId={item.value} />
+                  )}
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+        {horsePosts.length > 0 && (
+          <>
+            <hr />
+            <h2>Posts über {horse.name}</h2>
+            <ul>
+              {horsePosts.map((post) => (
+                <li key={post.slug}>
+                  <Link href={`/${post.slug}`}>{post.entry.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
       </div>
+    </div>
   )
 }
 
