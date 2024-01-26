86b41aad0ef35dd6cb5617696b31785015d1976d
Silver <xhexhajsilver@gmail.com>
2024-01-26 09:13:17 +0100

    Added horizontal scroll with snap effect on pferde gallery

--- app/(site)/gallery/page.tsx
+++ app/(site)/gallery/page.tsx
@@ -1,25 +1,25 @@
-import {reader} from "../../reader";
-import {ClickableImage} from "../../../components/clickable-image";
-import {formatDateLong} from "../../../composables/formatting";
+import { reader } from "../../reader";
+import { ClickableImage } from "../../../components/clickable-image";
+import { formatDateLong } from "../../../composables/formatting";
 
 export default async function AboutPage() {
   let albums = await reader.collections.albums.all()
   albums = albums.sort((a, b) => (a.entry.date < b.entry.date) ? 1 : ((b.entry.date < a.entry.date) ? -1 : 0))
 
   return (
-    <div className="mx-auto max-w-screen-xl my-20 px-8 space-y-16">
+    <div className="mx-auto max-w-screen-xl my-20 space-y-16">
       {albums.map((album) => (
-        <div className="space-y-4">
-          <div className="flex justify-between items-end">
+        <div className="space-y-8">
+          <div className="flex flex-col md:flex-row justify-between md:items-end px-8 md:space-y-0 space-y-4">
             <h2 className="text-2xl font-semibold">{album.slug}</h2>
             <span className="text-sm opacity-50">{formatDateLong(album.entry.date)}</span>
           </div>
-          <div className="grid grid-cols-6 gap-2">
+          <div className="md:grid md:grid-cols-6 flex space-x-4 md:space-x-0 overflow-x-scroll md:overflow-x-auto gap-2 px-8 snap-x snap-mandatory">
             {album.entry.photos.map(photo => (
               <ClickableImage
                 alt=""
                 src={photo}
-                className="transition cursor-pointer shadow-lg hover:scale-110 aspect-square object-cover"
+                className="transition cursor-pointer shadow-lg md:hover:scale-110 aspect-square object-cover snap-center"
               />
             ))}
           </div>
