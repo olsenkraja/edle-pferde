3e1a6b1bf97c1e29d7be1a0dfe041d6dd01ff077
Olsen Kraja <krajaolsen@gmail.com>
2024-06-09 15:22:50 +0200

    Added birthday, father and mother's father to horse list

--- app/(site)/horses/page.tsx
+++ app/(site)/horses/page.tsx
@@ -21,19 +21,34 @@ export default async function HorsesPage({params}: { params: { slug: string } })
                 />
                 <div className="space-y-4">
                   <div className="flex flex-wrap">
-                    <h2 className="mr-4 space-x-4">
-                      <span className="text-2xl font-bold">
-                        {horse.entry.full_name}
-                      </span>
-                      <span>
-                        ({horse.entry.birth_year})
-                      </span>
-                    </h2>
-                    {horse.entry.status === 'for-sale' && (
-                      <span className="bg-red-500 px-2 py-1 text-sm font-semibold uppercase text-white">
-                        For Sale
-                      </span>
-                    )}
+                    <div>
+                      <div className="flex items-center">
+                        <h2 className="mr-4 space-x-4">
+                          <span className="text-2xl font-bold">
+                            {horse.entry.full_name}
+                          </span>
+                        </h2>
+                        {horse.entry.status === 'for-sale' && (
+                          <span
+                            className="bg-red-500 text-red-100 px-2 py-1 text-sm font-semibold uppercase shadow rounded-full">
+                            For Sale
+                          </span>
+                        )}
+                      </div>
+                      <h3 className="text-gray-500">
+                        geb. {horse.entry.birth_year}
+                        {
+                          horse.entry.father.value
+                            ? ', ' + horse.entry.father.value
+                            : ''
+                        }
+                        {
+                          horse.entry.father.value && horse.entry.mothers_father.value
+                            ? '-' + horse.entry.mothers_father.value
+                            : ''
+                        }
+                      </h3>
+                    </div>
                   </div>
                   <p className="text-lg line-clamp-3">{horse.entry.bio}</p>
                 </div>
