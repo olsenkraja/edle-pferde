c2c39e78208f850cb1373889280e01f31c208c2f
Olsen Kraja <krajaolsen@gmail.com>
2024-06-16 22:41:55 +0200

    Replaced element icons

--- components/homepage/elements-section.tsx
+++ components/homepage/elements-section.tsx
@@ -2,46 +2,92 @@ export default async function ElementsSection() {
 
   return (
     <div className="bg-noble-200">
-      <div className="relative mx-auto grid grid-cols-2 md:grid-cols-4 max-w-screen-xl md:divide-x divide-x-0 divide-gray-800/20 px-8 py-8">
+      <div
+        className="relative mx-auto grid grid-cols-2 md:grid-cols-4 max-w-screen-xl md:divide-x divide-x-0 divide-gray-800/20 px-8 py-8">
         <div className="relative flex h-52 flex-1 flex-col items-center justify-center space-y-2">
           <span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                 stroke="currentColor" className="h-12 w-12">
-              <path strokeLinecap="round" strokeLinejoin="round"
-                    d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+            <svg
+              className="h-12 w-12"
+              xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"
+            >
+              <path fill="currentColor"
+                    d="M23.25 4c-1.262 0-2.344.457-3.125 1.125c-.781.668-1.297 1.535-1.656 2.344c-.563 1.265-.598 2.023-.657 2.5c-.457.191-1.636.656-2.812.656c-1.531 0-2.61-.625-4.625-.625c-1.246 0-2.367.46-3.156 1.281a4.552 4.552 0 0 0-.219.25a4.215 4.215 0 0 0-1.125-.156c-.707 0-1.336.352-1.813.813c-.476.46-.828 1.035-1.125 1.718C2.345 15.273 2 17.043 2 19h2c0-1.723.305-3.258.75-4.281c.223-.512.477-.887.688-1.094c.21-.207.351-.25.437-.25c.137 0 .188.02.281.031c-.078.364-.156.735-.156 1.125c0 2.387 1.441 3.832 1.969 4.313c-.004.379-.043 1.558-.782 2.562l-.187.25V28h2v-5.875c1.047-1.781 1-3.719 1-3.719L9.969 18l-.313-.281S8 16.172 8 14.53c0-.855.266-1.469.656-1.875c.39-.406.926-.656 1.719-.656c1.54 0 2.617.625 4.625.625c2.113 0 4.063-1 4.063-1l.5-.25l.062-.563s.102-1.28.656-2.53c.278-.626.684-1.223 1.157-1.626c.417-.36.898-.61 1.562-.656v1c1.508 0 2.094 1.406 2.094 1.406l.062.156l.125.157L27 10.406l-.188.219c-.44-.117-.94-.242-1.156-.313l-.343-.124l-.344.124c-.02.008-.594.145-1.032.063c-.437-.082-.761-.191-1-.844l-1.875.688c.153.418.368.77.625 1.062c-.09.465-.082.918-.03 1.344c.109.95.343 1.852.343 2.594c0 1.293-1.594 2.625-1.594 2.625l-.406.312V28h2v-8.906c.563-.434 2-1.676 2-3.875c0-1.106-.262-2.086-.344-2.813c-.004-.023.004-.039 0-.062c.63.101 1.059-.016 1.5-.125c.594.183 1.719.5 1.719.5l.594.156l.406-.469l.656-.719c.676-.75.653-1.91-.062-2.624l-1.75-1.782l-.032.032c-.156-.31-.66-1.231-1.687-1.813V4zM12 17v1c0 2.457-1.625 3.719-1.625 3.719L10 22v6h2v-5.156c.418-.395 1.254-1.352 1.719-3.219c.707.316 1.816.535 3.281.625V28h2v-9.656h-1c-2.031 0-3.094-.317-3.594-.563A1.386 1.386 0 0 1 14 17.5l-.219-.5z" />
             </svg>
           </span>
-          <span className="uppercase">Horses</span>
+          <span className="uppercase text-center">
+            familiäre
+            <br />
+            Pferdezucht
+          </span>
         </div>
         <div className="relative flex h-52 flex-1 flex-col items-center justify-center space-y-2">
           <span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                 stroke="currentColor" className="h-12 w-12">
-              <path strokeLinecap="round" strokeLinejoin="round"
-                    d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+            <svg
+              className="h-12 w-12"
+              xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"
+            >
+              <g fill="none" stroke="currentColor" stroke-width="1.5">
+                <path
+                  d="M17 8V6c0-1.886 0-2.828-.586-3.414C15.828 2 14.886 2 13 2h-2c-1.886 0-2.828 0-3.414.586C7 3.172 7 4.114 7 6v2" />
+                <path
+                  d="M10.564 5.783a3 3 0 0 1 2.872 0l4.794 2.614a3 3 0 0 1 1.564 2.634v4.938a3 3 0 0 1-1.564 2.634l-4.794 2.614a3 3 0 0 1-2.872 0l-4.795-2.614a3 3 0 0 1-1.563-2.634V11.03a3 3 0 0 1 1.563-2.634z" />
+                <path
+                  d="M11.146 11.523c.38-.682.57-1.023.854-1.023c.284 0 .474.341.854 1.023l.098.176c.108.194.162.29.246.355c.085.063.19.087.4.135l.19.043c.738.167 1.107.25 1.195.533c.088.282-.164.576-.667 1.164l-.13.152c-.143.168-.215.251-.247.355c-.032.103-.021.214 0 .438l.02.203c.076.784.114 1.177-.115 1.351c-.23.175-.576.016-1.267-.302l-.178-.083c-.197-.09-.295-.135-.399-.135c-.104 0-.202.045-.399.135l-.178.083c-.691.318-1.037.477-1.267.302c-.23-.174-.191-.567-.115-1.351l.02-.203c.021-.224.032-.335 0-.438c-.032-.104-.104-.187-.247-.355l-.13-.152c-.503-.588-.755-.882-.667-1.164c.088-.283.457-.366 1.195-.533l.19-.043c.21-.048.315-.072.4-.135c.084-.064.138-.161.246-.355z" />
+              </g>
             </svg>
           </span>
-          <span className="uppercase">Horses</span>
+          <span className="uppercase text-center">
+            individuelle
+            <br />
+            Ausbildung
+          </span>
         </div>
         <div className="relative flex h-52 flex-1 flex-col items-center justify-center space-y-2">
           <span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                 stroke="currentColor" className="h-12 w-12">
-              <path strokeLinecap="round" strokeLinejoin="round"
-                    d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+            <svg
+              className="h-12 w-12"
+              xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"
+            >
+              <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                <path d="M12 13a3 3 0 1 0 0-6a3 3 0 0 0 0 6" />
+                <path
+                  d="M17.8 13.938h-.011a7 7 0 1 0-11.464.144h-.016l.14.171q.15.19.3.371L12 21l5.13-6.248q.291-.314.54-.659z" />
+              </g>
             </svg>
           </span>
-          <span className="uppercase">Horses</span>
+          <span className="uppercase text-center">
+            im Herzen des
+            <br />
+            Hausruckviertels
+          </span>
         </div>
         <div className="relative flex h-52 flex-1 flex-col items-center justify-center space-y-2">
           <span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                 stroke="currentColor" className="h-12 w-12">
-              <path strokeLinecap="round" strokeLinejoin="round"
-                    d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+            <svg
+              className="h-12 w-12"
+              xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"
+            >
+              <rect width="416" height="384" x="48" y="80" fill="none" stroke="currentColor" stroke-linejoin="round"
+                    stroke-width="32" rx="48" />
+              <circle cx="296" cy="232" r="24" fill="currentColor" />
+              <circle cx="376" cy="232" r="24" fill="currentColor" />
+              <circle cx="296" cy="312" r="24" fill="currentColor" />
+              <circle cx="376" cy="312" r="24" fill="currentColor" />
+              <circle cx="136" cy="312" r="24" fill="currentColor" />
+              <circle cx="216" cy="312" r="24" fill="currentColor" />
+              <circle cx="136" cy="392" r="24" fill="currentColor" />
+              <circle cx="216" cy="392" r="24" fill="currentColor" />
+              <circle cx="296" cy="392" r="24" fill="currentColor" />
+              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"
+                    d="M128 48v32m256-32v32" />
+              <path fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32" d="M464 160H48" />
             </svg>
           </span>
-          <span className="uppercase">Horses</span>
+          <span className="uppercase text-center">
+            seit
+            <br />
+            1994
+          </span>
         </div>
       </div>
     </div>
