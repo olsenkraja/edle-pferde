5c2c9e20529918c7cfec9279096c88ed52735e5d
Olsen Kraja <krajaolsen@gmail.com>
2024-12-08 15:13:49 +0100

    Debugging API responses in vercel

--- app/api/[collectionName]/route.ts
+++ app/api/[collectionName]/route.ts
@@ -1,6 +1,7 @@
 import {NextResponse} from "next/server";
 import {reader} from "../../reader";
 
+export const dynamic = 'force-dynamic'
 export async function GET(request: Request, {params}) {
     try {
         const { collectionName } = params;
