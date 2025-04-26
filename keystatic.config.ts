ffd76a2af3035a87de25417618be095a6516c437
Olsen Kraja <krajaolsen@gmail.com>
2025-04-27 01:29:49 +0200

    Migration of horses

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -38,5 +38,141 @@ export default config({
         }),
       },
     }),
+    horses: collection({
+      columns: ['full_name', 'birth_year'],
+      label: 'Horses',
+      slugField: 'full_name',
+      path: 'src/content/horses/*',
+      format: {contentField: 'content'},
+      schema: {
+        full_name: fields.slug({name: {label: 'Full name'}}),
+        family: fields.text({label: 'Family'}),
+        gender: fields.select({
+          label: 'Gender',
+          options: [
+            { label: 'Hengst', value: 'Hengst' },
+            { label: 'Wallach', value: 'Wallach' },
+            { label: 'Stute', value: 'Stute' },
+          ],
+          defaultValue: 'Hengst'
+        }),
+        profile_picture: fields.image({
+          label: 'Profile picture',
+          directory: 'src/assets/profile_picture/horses',
+          publicPath: '@assets/profile_picture/horses/',
+          validation: {
+            isRequired: true
+          },
+        }),
+        profile_picture_alt_text: fields.text({label: 'Profile picture / Alt text'}),
+        status: fields.select({
+          label: 'Status',
+          options: [
+            { label: 'Active', value: 'active' },
+            { label: 'Inactive', value: 'inactive' },
+            { label: 'Fro sale', value: 'for-sale' },
+          ],
+          defaultValue: 'active'
+        }),
+        birth_year: fields.text({
+          label: 'Birth year',
+        }),
+        breed: fields.text({label: 'Breed'}),
+        size: fields.text({label: 'Size'}),
+        color: fields.text({label: 'Color'}),
+        bio: fields.text({label: 'Bio', multiline: true}),
+        content: fields.mdx({
+          label: 'Content',
+          options: {
+            image: {
+              directory: 'src/assets/images/horses',
+              publicPath: '../../assets/images/horses/',
+            },
+          },
+        }),
+        father: fields.conditional(
+          fields.checkbox({ label: 'Is the father our horse?' }),
+          {
+            true: fields.relationship({
+              label: 'Father',
+              collection: 'horses'
+            }),
+            false: fields.text({
+              label: 'Father'
+            }),
+          }
+        ),
+        mother: fields.conditional(
+          fields.checkbox({ label: 'Is the mother our horse?' }),
+          {
+            true: fields.relationship({
+              label: 'Mother',
+              collection: 'horses'
+            }),
+            false: fields.text({
+              label: 'Mother'
+            }),
+          }
+        ),
+        fathers_father: fields.conditional(
+          fields.checkbox({ label: 'Is the father\'s father our horse?' }),
+          {
+            true: fields.relationship({
+              label: 'Father\'s father',
+              collection: 'horses'
+            }),
+            false: fields.text({
+              label: 'Father\'s father'
+            }),
+          }
+        ),
+        fathers_mother: fields.conditional(
+          fields.checkbox({ label: 'Is the father\'s mother our horse?' }),
+          {
+            true: fields.relationship({
+              label: 'Father\'s mother',
+              collection: 'horses'
+            }),
+            false: fields.text({
+              label: 'Father\'s mother'
+            }),
+          }
+        ),
+        mothers_father: fields.conditional(
+          fields.checkbox({ label: 'Is the mother\'s father our horse?' }),
+          {
+            true: fields.relationship({
+              label: 'Mother\'s father',
+              collection: 'horses'
+            }),
+            false: fields.text({
+              label: 'Mother\'s father'
+            }),
+          }
+        ),
+        mothers_mother: fields.conditional(
+          fields.checkbox({ label: 'Is the mother\'s mother our horse?' }),
+          {
+            true: fields.relationship({
+              label: 'Mother\'s mother',
+              collection: 'horses'
+            }),
+            false: fields.text({
+              label: 'Mother\'s mother'
+            }),
+          }
+        ),
+        children: fields.array(
+          fields.relationship({
+            label: 'Children',
+            collection: 'horses'
+          }),
+          {
+            label: 'Children',
+            itemLabel: (item) => item.value || 'Please select an horse',
+          }
+        ),
+      },
+    }),
   },
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -10,4 +10,13 @@ const articles = defineCollection({
   }),
 });
 
-export const collections = { articles };
+const horses = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    full_name: z.string(),
+    birth_year: z.string(),
+    profile_picture: z.string(),
+  }),
+});
+
+export const collections = { articles, horses };
