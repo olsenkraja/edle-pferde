26923d38b877e24606e5ed00ee9a1d83a4c03a76
Olsen Kraja <krajaolsen@gmail.com>
2025-04-27 20:43:20 +0200

    Added horses details page

--- src/content/config.ts
+++ src/content/config.ts
@@ -14,8 +14,38 @@ const horses = defineCollection({
   // Type-check frontmatter using a schema
   schema: z.object({
     full_name: z.string(),
+    family: z.string().nullable().optional(),
     birth_year: z.string(),
     profile_picture: z.string(),
+    breed: z.string(),
+    size: z.string(),
+    color: z.string(),
+    bio: z.string(),
+    father: z.object({
+      discriminant: z.boolean(),
+      value: z.string().nullable().optional()
+    }).nullable().optional(),
+    mother: z.object({
+      discriminant: z.boolean(),
+      value: z.string().nullable().optional()
+    }).nullable().optional(),
+    fathers_father: z.object({
+      discriminant: z.boolean(),
+      value: z.string().nullable().optional()
+    }).nullable().optional(),
+    fathers_mother: z.object({
+      discriminant: z.boolean(),
+      value: z.string().nullable().optional()
+    }).nullable().optional(),
+    mothers_father: z.object({
+      discriminant: z.boolean(),
+      value: z.string().nullable().optional()
+    }).nullable().optional(),
+    mothers_mother: z.object({
+      discriminant: z.boolean(),
+      value: z.string().nullable().optional()
+    }).nullable().optional(),
+    children: z.array(z.string()),
   }),
 });
 
